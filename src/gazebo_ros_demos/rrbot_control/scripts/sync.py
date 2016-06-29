79686b7e5d562ad06a87308debff6424bba0c273
somal <somal1996@rambler.ru>
2016-06-29 14:12:16 +0300

    New version with new models

--- src/gazebo_ros_demos/rrbot_control/scripts/sync.py
+++ src/gazebo_ros_demos/rrbot_control/scripts/sync.py
@@ -5,12 +5,12 @@ from control_msgs.msg import JointControllerState
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.process_value)
-    pub = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
-    pub.publish(data.set_point)
+    pub = rospy.Publisher('/rrbot/joint_left_flipper_controller/command', Float64, queue_size=10)
+    pub.publish(-data.set_point)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/rrbot/joint1_position_controller/state", JointControllerState, callback)
+    rospy.Subscriber("/rrbot/joint_right_flipper_controller/state", JointControllerState, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
