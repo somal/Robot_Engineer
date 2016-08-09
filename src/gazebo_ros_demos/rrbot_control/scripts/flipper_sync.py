112637aad1f148faa7a65df39f3cb436cf0a1d90
somal <somal1996@rambler.ru>
2016-08-09 22:34:14 +0300

    update 9th august

--- src/gazebo_ros_demos/rrbot_control/scripts/flipper_sync.py
+++ src/gazebo_ros_demos/rrbot_control/scripts/flipper_sync.py
@@ -6,14 +6,14 @@ from control_msgs.msg import JointControllerState
 
 # Left flipper is a mirror of right flipper, so angles of flippers are inversed (i,-i)
 def callback_for_right_flipper(data):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.process_value)
     pub = rospy.Publisher('/rrbot/joint_left_flipper_controller/command', Float64, queue_size=10)
-    pub.publish(-data.set_point) 
-    
+    pub.publish(-data.set_point)
+
+
 def callback_for_left_flipper(data):
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.process_value)
     pub = rospy.Publisher('/rrbot/joint_right_flipper_controller/command', Float64, queue_size=10)
     pub.publish(-data.set_point)
+
 
 def listener():
     rospy.init_node('listener', anonymous=True)
@@ -21,6 +21,7 @@ def listener():
     rospy.Subscriber("/rrbot/joint_left_flipper_controller/state", JointControllerState, callback_for_left_flipper)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
