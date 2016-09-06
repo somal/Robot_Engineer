d5bbdb9af0fd044ac657827623396d250e3d8424
somal <somal1996@rambler.ru>
2016-09-06 23:27:51 +0300

    Big update: Rebuild 3d model

--- src/gazebo_ros_demos/rrbot_control/scripts/flipper_sync.py
+++ src/gazebo_ros_demos/rrbot_control/scripts/flipper_sync.py
@@ -7,12 +7,12 @@ from control_msgs.msg import JointControllerState
 # Left flipper is a mirror of right flipper, so angles of flippers are inversed (i,-i)
 def callback_for_right_flipper(data):
     pub = rospy.Publisher('/rrbot/joint_left_flipper_controller/command', Float64, queue_size=10)
-    pub.publish(-data.set_point)
+    pub.publish(data.set_point)
 
 
 def callback_for_left_flipper(data):
     pub = rospy.Publisher('/rrbot/joint_right_flipper_controller/command', Float64, queue_size=10)
-    pub.publish(-data.set_point)
+    pub.publish(data.set_point)
 
 
 def listener():
