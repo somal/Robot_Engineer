a76b449469baf20b18e176deb70ab0ad255622c5
somal <somal1996@rambler.ru>
2016-09-09 14:00:03 +0300

    Add cube

--- src/gazebo_ros_demos/rrbot_control/scripts/left_sync.py
+++ src/gazebo_ros_demos/rrbot_control/scripts/left_sync.py
@@ -3,14 +3,19 @@ import rospy
 from std_msgs.msg import Float64
 
 
+def divide(a,b):
+    return Float64(a.data/b.data)
+
 # Left flipper is a mirror of right flipper, so angles of flippers are inversed (i,-i)
 def callback(data):
-#    rospy.loginfo(data) 
+#    rospy.loginfo(data)
+    coef=Float64(0.7)
+    res=divide(data,coef)
     pub = rospy.Publisher('/rrbot/joint_left_flipper_wheel_controller/command', Float64, queue_size=10)
-    pub.publish(data) 
-    
+    pub.publish(res)
+
     pub = rospy.Publisher('/rrbot/joint_left_rear_base_link_wheel_controller/command', Float64, queue_size=10)
-    pub.publish(data) 
+    pub.publish(res)
 
 
 def listener():
