05ff4e93e14236196c7b34c312c50eba33db0afe
somal <somal1996@rambler.ru>
2016-09-10 21:08:19 +0300

    Add normal src

---
+++ src/engineer/eng_control/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/somal/catkin_ws/src/gazebo_ros_demos/rrbot_control/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/opt/ros/indigo/include"
+export LD_LIBRARY_PATH="/home/somal/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/opt/ros/indigo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/somal/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/somal/catkin_ws/src/gazebo_ros_demos/rrbot_control/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/somal/catkin_ws/src/gazebo_ros_demos/rrbot_control:$ROS_PACKAGE_PATH"
