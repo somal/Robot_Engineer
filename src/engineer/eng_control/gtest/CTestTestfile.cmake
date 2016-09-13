a16b82f4d30a642a08f85c126678d60d3c515474
Roman Lavrenov <r.lavrenov@innopolis.ru>
2016-09-13 14:01:10 +0300

    del

--- src/engineer/eng_control/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/somal/catkin_ws/src/gazebo_ros_demos/rrbot_control/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
